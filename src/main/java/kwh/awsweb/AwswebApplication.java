473eb3cbc5bfff3d28c52ae19703f28c1c430e1f
Kim Wan ho <readerc866@gmail.com>
2024-06-04 14:57:41 +0900

    Initial commit

---
+++ src/main/java/kwh/awsweb/AwswebApplication.java
@@ -0,0 +1,13 @@
+package kwh.awsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwswebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwswebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kwh/awsweb/AwswebApplicationTests.java
@@ -0,0 +1,13 @@
+package kwh.awsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
