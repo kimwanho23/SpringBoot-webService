29596bfcbeb5d85cbce3f5ad871b7a4c30d484af
Kim Wan ho <readerc866@gmail.com>
2024-06-08 00:32:28 +0900

    error commit

--- src/test/java/kwh/awsweb/AwswebApplicationTests.java
+++ src/test/java/kwh/awsweb/AwswebApplicationTests.java
@@ -1,9 +1,7 @@
 package kwh.awsweb;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootTest
 class AwswebApplicationTests {
