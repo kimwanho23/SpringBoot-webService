d1e24e5e5a21c7b7c11dce8aaf4720cc37a726ee
Kim Wan ho <readerc866@gmail.com>
2024-06-09 15:52:07 +0900

    security

--- src/main/java/kwh/awsweb/config/auth/OAuthAttributes.java
+++ src/main/java/kwh/awsweb/config/auth/OAuthAttributes.java
@@ -4,7 +4,6 @@ import kwh.awsweb.domain.user.Role;
 import kwh.awsweb.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
-
 import java.util.Map;
 
 @Getter
@@ -16,9 +15,7 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes,
-                           String nameAttributeKey, String name,
-                           String email, String picture) {
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -26,15 +23,13 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    //
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName,
-                                     Map<String, Object> attributes) {
-            if ("naver".equals(registrationId)) {
-                return ofNaver("id", attributes);
-            }
-        return ofGoogle(userNameAttributeName, attributes);
+    // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야 한다.
+    public static OAuthAttributes of(String registrationId, String userNAmeAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) return ofNaver("id", attributes);
+        return ofGoogle(userNAmeAttributeName, attributes);
     }
 
+    // google login OAuth
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
@@ -45,9 +40,9 @@ public class OAuthAttributes {
                 .build();
     }
 
+    // naver login OAuth
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");
-
         return OAuthAttributes.builder()
                 .name((String) response.get("name"))
                 .email((String) response.get("email"))
@@ -57,7 +52,10 @@ public class OAuthAttributes {
                 .build();
     }
 
-    public User toEntity(){
+    // User 엔티티를 생성 후 OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때이다.
+    // 가입할 때의 기본 권한은 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용한다.
+    // OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
@@ -66,4 +64,3 @@ public class OAuthAttributes {
                 .build();
     }
 }
-
