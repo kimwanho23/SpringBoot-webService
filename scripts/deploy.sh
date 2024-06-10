1978a3807f80ce1b32fe2338fbba435ec1173e28
Kim Wan ho <readerc866@gmail.com>
2024-06-10 22:02:57 +0900

    codedeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,18 +7,17 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl freelec-springboot3-webservice | grep jar | awk '{print $1}')
-echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
-
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
+   echo "> kill -15 $CURRENT_PID"
+   kill -15 $CURRENT_PID
+   sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
